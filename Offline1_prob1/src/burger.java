dc694e341ed88df4d6455330b1d5769e44d33edd
nadiaanjum <nadia4anjum421@gmail.com>
2018-04-30 16:04:27 +0600

    complete prob 1

--- Offline1_prob1/src/burger.java
+++ Offline1_prob1/src/burger.java
@@ -3,6 +3,19 @@ import java.util.List;
 
 public class burger {
     private List<burgerItem> item=new ArrayList<burgerItem>();
+    public void addburger(burgerItem bur)
+    {
+        item.add(bur);
+    }
+    public void show()
+    {
+        for(burgerItem x:item)
+        {
+            System.out.println(x.Name()+"-"+x.Patty()+" "+x.sauce()+" "+x.chesse()+":"+x.price());
+
+
+        }
+    }
 
 
 }
--- Offline1_prob1/src/chef.java
+++ Offline1_prob1/src/chef.java
@@ -7,6 +7,17 @@ public class chef {
         Scanner scanner=new Scanner(System.in);
         int num=scanner.nextInt();
         burger b=new burger();
+        for (int i=0;i<num;i++)
+        {
+            System.out.println("enter choice: 1.chicken 2.beef");
+            int option = scanner.nextInt();
+            if(option==1)
+            {
+                b.addburger(new chicken());
+            }
+            else
+                b.addburger(new beef());
+        }
         return b;
     }
 }
